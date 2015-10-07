e64c8acce78f3728aba268f425bdde351790caf4
Ashish Jain <jain.ashishjain1@gmail.com>
2015-10-07 13:50:11 -0500

    check for null.

--- src/com/iowa/gbeer/CopyGenomeDirectoryToNewFolder.java
+++ src/com/iowa/gbeer/CopyGenomeDirectoryToNewFolder.java
@@ -12,17 +12,20 @@ import org.apache.commons.io.FileUtils;
 public class CopyGenomeDirectoryToNewFolder {
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader("/home/jain/Gram_Positive_Bacteria_Study/Organisms_Lists_from_PATRIC/Bacillus/PatricOrgsGenomesMap.txt"));
-		String basePath = "/home/jain/Gram_Positive_Bacteria_Study/Bacillus_Genomes_Path/";
+		BufferedReader br = new BufferedReader(new FileReader("/home/jain/Gram_Positive_Bacteria_Study/Organisms_Lists_from_PATRIC/Firmicutes/PatricOrgsGenomesMap.txt"));
+		String basePath = "/home/jain/Gram_Positive_Bacteria_Study/Fericuites_Genomes_Path/";
 		String line = br.readLine();
 		Set<String> dirPaths = new HashSet<>();
 		while(line!=null)
 		{
 			String lineData[] = line.split("\t");
 			String folderPath = lineData[1].trim();
-			dirPaths.add(folderPath);
-			String folderName = folderPath.split("/")[4];
-			FileUtils.copyDirectory(new File(folderPath), new File(basePath+folderName));
+			if(!folderPath.equals("null"))
+			{
+				dirPaths.add(folderPath);
+				String folderName = folderPath.split("/")[4];
+				FileUtils.copyDirectory(new File(folderPath), new File(basePath+folderName));
+			}
 			line = br.readLine();
 		}
 		br.close();
