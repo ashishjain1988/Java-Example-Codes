3bfd76080bec5fc12ca1026f18ba1dde05b828e9
Ashish Jain <jain.ashishjain1@gmail.com>
2015-11-06 15:46:47 -0600

    parsing file for creating bed file for GO analysis.

---
+++ src/com/iowa/tutejalab/CreateBedFileForGREAT.java
@@ -0,0 +1,18 @@
+package com.iowa.tutejalab;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CreateBedFileForGREAT {
+
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(""));
+		
+		
+		br.close();
+		
+		
+	}
+}
