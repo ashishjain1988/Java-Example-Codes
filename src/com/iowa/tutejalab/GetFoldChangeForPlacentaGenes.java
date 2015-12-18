8d8ba19092e7b3e82bfe2ad4f4f3835b5597f02e
Ashish Jain <jain.ashishjain1@gmail.com>
2015-12-18 15:46:11 -0600

    commit for all the files in a folder.

--- src/com/iowa/tutejalab/GetFoldChangeForPlacentaGenes.java
+++ src/com/iowa/tutejalab/GetFoldChangeForPlacentaGenes.java
@@ -4,6 +4,7 @@ package com.iowa.tutejalab;
  * 
  */
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,30 +14,45 @@ import java.util.List;
 public class GetFoldChangeForPlacentaGenes {
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader("/home/jain/Downloads/Combined_GeneNames.txt"));
-		List<String> geneList = new ArrayList<>();
-		String line = br.readLine();
-		String output = "/home/jain/Placenta_Geo_Dataset/Cuffdiff_Output_New/Cuffdiff_Output_New/InVivo_InVitro_Diff_Study/Gene_E-IV_NE-Cell";
-		while(line!=null)
+		File[] files = new File("/home/jain/Placenta_Geo_Dataset/Placenta_Different_Stages_Analysis/Cuffdiff_Output/").listFiles();
+		//PrintWriter pw1 = new PrintWriter("Placenta_Results.txt");
+		for (File f:files)
 		{
-			geneList.add(line.trim());
-			line = br.readLine();
-		}
-		br.close();
-		br = new BufferedReader(new FileReader(output+"/gene_exp.diff"));
-		line = br.readLine();
-		PrintWriter pw = new PrintWriter(output+"/Placenta_genes_exp_1.txt");
-		pw.println("Gene\tIn Vivo Exp\tIn Vitro Exp\tFold Change\tLog(Fold Change)");
-		while(line!=null)
-		{
-			String lineData[] = line.split("\t");
-			if(geneList.contains(lineData[2]) && lineData[13].equals("yes"))
+			if(f.isDirectory())
 			{
-				pw.println(lineData[2]+"\t"+lineData[7]+"\t"+lineData[8]+"\t"+(Float.valueOf(lineData[7])/Float.valueOf(lineData[8]))+"\t"+lineData[9]);
+				File[] diffFiles=new File(f.getAbsolutePath()+"/Analysis/").listFiles();
+				for (File f1:diffFiles)
+				{
+					if(f1.getName().endsWith("genes.txt"))
+					{
+						BufferedReader br = new BufferedReader(new FileReader("/home/jain/Downloads/Combined_GeneNames.txt"));
+						List<String> geneList = new ArrayList<>();
+						String line = br.readLine();
+						String output = "/home/jain/Placenta_Geo_Dataset/Placenta_Different_Stages_Analysis/Cuffdiff_Output/E9.5_Female_In-Vitro_TGC/Analysis";
+						while(line!=null)
+						{
+							geneList.add(line.trim());
+							line = br.readLine();
+						}
+						br.close();
+						br = new BufferedReader(new FileReader(output+"/DEG_Signi_Genes.txt"));
+						line = br.readLine();
+						PrintWriter pw = new PrintWriter(output+"/Placenta_genes_exp_1.txt");
+						pw.println("Gene\tIn Vivo Exp\tIn Vitro Exp\tFold Change\tLog(Fold Change)");
+						while(line!=null)
+						{
+							String lineData[] = line.split("\t");
+							if(geneList.contains(lineData[2]) && lineData[13].equals("yes"))
+							{
+								pw.println(lineData[2]+"\t"+lineData[7]+"\t"+lineData[8]+"\t"+(Float.valueOf(lineData[7])/Float.valueOf(lineData[8]))+"\t"+lineData[9]);
+							}
+							line = br.readLine();
+						}
+						br.close();
+						pw.close();
+					}
+				}
 			}
-			line = br.readLine();
 		}
-		br.close();
-		pw.close();
 	}
 }
